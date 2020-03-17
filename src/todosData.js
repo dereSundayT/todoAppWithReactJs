a720eb6a80af462d3193aa94dcbf7df7a8d35405
dereSundayT <sundaytoluwanidere@gmail.com>
2020-03-17 11:53:26 +0000

    Simple todo app with react

---
+++ src/todosData.js
@@ -0,0 +1,41 @@
+const todosData =
+[    
+    {
+        id:1,
+        text: " ",
+        completed:true
+
+    },
+    {
+        id:2,
+        text: " ",
+        completed:true
+
+    },
+    {
+        id:3,
+        text: " ",
+        completed:false
+    },
+    {
+        id:4,
+        text: " ",
+        completed:false
+
+    },
+    {
+        id:5,
+        text: " ",
+        completed:false,
+
+    },
+    {
+        id:6,
+        text: " ",
+        completed:false
+    }
+   
+
+]
+
+export default todosData
